c2fee7b1223a95a7b2cabc062c98593840d6748d
Rafael <rafael.vasel@gmail.com>
2018-08-18 20:24:38 -0300

    corrigido nome comercial

--- funcoes_cademeuremedio.py
+++ funcoes_cademeuremedio.py
@@ -24,23 +24,30 @@ def lista_medicamentos_sus(termo):
         for principio in principios:
             result = dfListaRename[dfListaRename["remedio"].str.contains(principio)]  # .split(';')[0].split(' ')[0])]
             if not result.empty:
-                result['comercial'] = termo
+                result['comercial'] = busca_nome_comercial(termo)
                 if dfl.empty:
                     dfl = result
                 else:
-                    dfl = pd.concat(dfl, result)
+                    dfl = pd.concat([dfl, result])
         if not dfl.empty:
             return dfl[['id', 'remedio', 'comercial']].head(10)
     else:
         return dfl[['id', 'remedio', 'comercial']].head(10)
-
     return pd.DataFrame({"ERROR": termo + ' não encontrado.'}, index=[0])
-
 
 def busca_principio_por_nome_comercial(termo):
     dfl = dfListaProdutos[dfListaProdutos['PRODUTO'].str.contains(termo)]
-    return dfl['PRINCIPIO ATIVO']
+    if (dfl.empty):
+        return pd.DataFrame();
+    else:
+        return dfl['PRINCIPIO ATIVO']
 
+
+def busca_nome_comercial(termo):
+    try:
+        return dfListaProdutos[dfListaProdutos['PRODUTO'].str.contains(termo)]['PRODUTO'].iloc[0]
+    except:
+        return pd.DataFrame();
 
 def todos_remedios(termo):
     termo = normaliza(termo)
@@ -49,16 +56,17 @@ def todos_remedios(termo):
         dfl = dfListaProdutos[dfListaProdutos['PRODUTO'].str.contains(termo)]
     return dfl.head(100)
 
-def lista_por_nome_comercial(termo):
-    termo = normaliza(termo)
-    dfl = dfListaProdutos[dfListaProdutos['PRODUTO'].str.contains(termo)]
-    if (dfl.empty):
-        return pd.DataFrame(['0', termo + ' não encontrado', ''])
-    dfl = retira_nao_tem_no_sus(dfl)
-    if (dfl.empty):
-        return pd.DataFrame(['0', termo + ' não disponivel no SUS', ''])
-    else:
-        return dfl.head(10)
+
+# def lista_por_nome_comercial(termo):
+#     termo = normaliza(termo)
+#     dfl = dfListaProdutos[dfListaProdutos['PRODUTO'].str.contains(termo)]
+#     if (dfl.empty):
+#         return pd.DataFrame(['0', termo + ' não encontrado', ''])
+#     dfl = retira_nao_tem_no_sus(dfl)
+#     if (dfl.empty):
+#         return pd.DataFrame(['0', termo + ' não disponivel no SUS', ''])
+#     else:
+#         return dfl.head(10)
 
 def retira_nao_tem_no_sus(lista):
     for row in lista.iterrows():
@@ -118,7 +126,8 @@ def gera_dados(qtde):
     return 'ok'
 
 
-#port = int(os.environ.get("PORT", 7777))
+# if __name__ == "__main__":
+# port = int(os.environ.get("PORT", 7777))
 # if (port == 7777):
 df = pd.read_json('listaISO.json')  # , encoding='UTF8')
 
@@ -134,13 +143,12 @@ dfListaRename['PRINCIPIO'] = dfListaRename["PRINCIPIO ATIVO"].apply(normaliza)
 dfListaRename['remedio'] = dfListaRename['PRINCIPIO'] + dfListaRename["COMPOSICAO"].apply(normaliza) + dfListaRename[
     "COMPONENTE"].apply(normaliza)
 
-
-
 dfListaRename['id'] = dfListaRename.index
 dfListaRename.rename(index=str, columns={"COMPONENTE": "APRESENTACAO", "COMPOSICAO": "PRODUTO"})
 dfListaRename['comercial'] = ""
 
 denuncias=dict()
+nomeProduto =""
 # print (lista_medicamentos('tylenol'))
 #print(lista_medicamentos('CITRATO'))
 
